8e7bbf3e6bb358d62eefe18f8da9460e3259c50b
brian.musili <brian.musili@systechafrica.com>
2024-09-30 09:28:20 +0300

    Project Setup

---
+++ src/test/java/dev/cee/dreamshops/DreamshopsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cee.dreamshops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamshopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
