c6c5fb85b02ef0f083350016b171b0facd3af2f3
brian.musili <brian.musili@systechafrica.com>
2024-10-01 18:03:16 +0300

    Category service and repository implementation

---
+++ src/main/java/dev/cee/dreamshops/service/category/CategoryService.java
@@ -0,0 +1,62 @@
+package dev.cee.dreamshops.service.category;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.apache.coyote.BadRequestException;
+import org.springframework.stereotype.Service;
+
+import dev.cee.dreamshops.exceptions.AlreadyExistException;
+import dev.cee.dreamshops.exceptions.ResourceNotFoundException;
+import dev.cee.dreamshops.model.Category;
+import dev.cee.dreamshops.repository.category.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService implements ICategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    public Category getCategoryById(Long id) {
+        return categoryRepository
+                .findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Category Not Found"));
+    }
+
+    @Override
+    public Category getCategoryByName(String name) {
+        return categoryRepository
+                .findByName(name);
+    }
+
+    @Override
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    @Override
+    public Category addCategory(Category category) {
+        return Optional.of(category)
+                .filter(c -> !categoryRepository.existsByName(c.getName()))
+                .map(categoryRepository::save)
+                .orElseThrow(() -> new AlreadyExistException(category.getName() + " Category Already Exist"));
+    }
+
+    @Override
+    public Category updateCategory(Category category, Long id) {
+        return Optional.ofNullable(this.getCategoryById(id))
+                .map(oldCategory -> {
+                    oldCategory.setName(category.getName());
+                    return categoryRepository.save(oldCategory);
+                }).orElseThrow(() -> new ResourceNotFoundException("Category Not Found"));
+    }
+
+    @Override
+    public void deleteCategory(Category category) {
+        categoryRepository
+                .findById(category.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("Category Not Found"));
+    }
+}
---
+++ src/main/java/dev/cee/dreamshops/service/category/ICategoryService.java
@@ -0,0 +1,17 @@
+package dev.cee.dreamshops.service.category;
+
+import java.util.List;
+
+import dev.cee.dreamshops.model.Category;
+
+public interface ICategoryService {
+
+    Category getCategoryById(Long id);
+    Category getCategoryByName(String name);
+    List<Category> getAllCategories();
+    Category addCategory(Category category);
+    Category updateCategory(Category category, Long id);
+    void deleteCategory(Category category);
+
+
+}
