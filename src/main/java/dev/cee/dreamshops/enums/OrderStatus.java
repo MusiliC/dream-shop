6c16881f517de329c2132355430f508f29c60321
brian.musili <brian.musili@systechafrica.com>
2024-10-17 07:46:10 +0300

    Order and Order item entity

---
+++ src/main/java/dev/cee/dreamshops/enums/OrderStatus.java
@@ -0,0 +1,10 @@
+package dev.cee.dreamshops.enums;
+
+public enum OrderStatus {
+
+    PENDING,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED,
+    CANCELLED,
+}
---
+++ src/main/java/dev/cee/dreamshops/model/OrderItem.java
@@ -0,0 +1,50 @@
+package dev.cee.dreamshops.model;
+
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int quantity;
+
+    private BigDecimal price;
+
+    @ManyToOne
+    @JsonBackReference
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JsonIgnore
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public OrderItem(Order order, Product product, int quantity, BigDecimal price) {
+        this.order = order;
+        this.product = product;
+        this.quantity = quantity;
+        this.price = price;
+    }
+}
