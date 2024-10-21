9d1efeb8e855598d642746093aa3db0e4b884d7e
brian.musili <brian.musili@systechafrica.com>
2024-10-21 07:07:30 +0300

    Order item implementation

--- src/main/java/dev/cee/dreamshops/service/order/OrderService.java
+++ src/main/java/dev/cee/dreamshops/service/order/OrderService.java
@@ -2,13 +2,18 @@ package dev.cee.dreamshops.service.order;
 
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 import org.springframework.stereotype.Service;
 
+import dev.cee.dreamshops.enums.OrderStatus;
 import dev.cee.dreamshops.exceptions.ResourceNotFoundException;
+import dev.cee.dreamshops.model.Cart;
 import dev.cee.dreamshops.model.Order;
 import dev.cee.dreamshops.model.OrderItem;
+import dev.cee.dreamshops.model.Product;
 import dev.cee.dreamshops.repository.order.OrderRepository;
+import dev.cee.dreamshops.repository.product.ProductRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -16,6 +21,8 @@ import lombok.RequiredArgsConstructor;
 public class OrderService implements OrderServiceI {
 
     private final OrderRepository orderRepository;
+
+    private final ProductRepository productRepository;
 
     @Override
     public Order placeOrder(Long userId, Order order) {
@@ -25,6 +32,30 @@ public class OrderService implements OrderServiceI {
     @Override
     public Order getOrderById(Long orderId) {
         return orderRepository.findById(orderId).orElseThrow(() -> new ResourceNotFoundException("Order not found"));
+    }
+
+    private Order createOrder(Cart cart){
+        Order order = new Order();
+        //set user
+        order.setOrderStatus(OrderStatus.PENDING);
+        order.setOrderDate(LocalDate.now());
+        return order;
+    }
+
+    private List<OrderItem> createOrderItems(Order order, Cart cart){
+        return cart.getItems()
+                .stream()
+                .map((cartItem -> {
+                    Product product = cartItem.getProduct();
+                    product.setInventory(product.getInventory() - cartItem.getQuantity());
+                    productRepository.save(product);
+                    return new OrderItem(
+                            order,
+                            product,
+                            cartItem.getQuantity(),
+                            cartItem.getUnitPrice()
+                    );
+                })).toList();
     }
 
     private BigDecimal calculateTotalAmount(List<OrderItem> orderItems) {
